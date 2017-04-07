2bb2cb5273cebb0dfaee7ec1269f3249410f9977
JCEntrena <jentrena@correo.ugr.es>
2017-04-07 19:57:27 +0200

    Multiplicación de matrices secuencial.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,38 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
-#include <mpi.h>
+//#include <mpi.h>
 #include <time.h>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 
 using namespace std;
+
+/**
+ * Función de lectura de archivos.
+ * Lee una matriz de un archivo y la devuelve.
+ */
+vector< vector<float> > readMatrix(string file){
+  ifstream ifs;
+  ifs.open(file, ifstream::in);
+
+  // Leemos elementos
+  int rows, cols;
+  ifs >> rows;
+  ifs >> cols;
+
+  vector< vector<float> > resultado(rows, vector<float>(cols, 0));
+
+  // Lectura.
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      ifs >> resultado.at(i).at(j);
+
+  ifs.close();
+
+  return resultado;
+}
 
 int main(int argc, char *argv[]){
 
@@ -20,50 +45,48 @@ int main(int argc, char *argv[]){
   string file1, file2;
 
   // Lectura de argumentos
-  istringstream iss( argv[1] );
-  iss >> file1;
-  iss >> file2;
-
-  // Abrir los ficheros y leer las matrices
-  ifstream ifs;
-  // Matriz 1
-  ifs.open(file1, ifstream::in);
-
-  // Leemos filas y columnas.
-  int rows1, cols1;
-  ifs >> rows1;
-  ifs >> cols1;
+  file1 = argv[1];
+  file2 = argv[2];
 
   // Declaración
-  vector< vector<float>> matrix1 (rows1, vector<float>(cols1, 0));
+  vector< vector<float>> matrix1 = readMatrix(file1);
+  vector< vector<float>> matrix2 = readMatrix(file2);
 
-  // Lectura.
+  // Tomamos las filas y columnas. Las necesitamos para definir la matrix producto.
+  int rows1 = matrix1.size();
+  int cols1 = matrix1.at(0).size();
+  int rows2 = matrix2.size();
+  int cols2 = matrix2.at(0).size();
+
+  if (cols1 != rows2){
+    cerr << "Las matrices no se pueden multiplicar." << endl;
+    exit(-1);
+  }
+
+
+  // Matriz producto. Inicializada a 0.
+  vector< vector<float>> matrixf(rows1, vector<float>(cols2, 0));
+
+  // SECUENCIAL. CALCULAMOS EL PRODUCTO Y LO ESCRIBIMOS EN UN ARCHIVO.
+
   for (int i = 0; i < rows1; ++i)
-    for (int j = 0; j < cols1; ++j)
-      ifs >> matrix1.at(i).at(j);
+    for (int k = 0; k < cols1; ++k)
+      for (int j = 0; j < cols2; ++j)
+        matrixf.at(i).at(j) += matrix1.at(i).at(k) * matrix2.at(k).at(j);
 
-  fs.close();
 
-  // Matriz 2.
-  fs.open(file2, ifstream::in);
+  ofstream ofs("Resultado.txt", ofstream::out);
 
-  // Leemos filas y columnas.
-  int rows2, cols2;
-  ifs >> rows2;
-  ifs >> cols2;
+  ofs << rows1 << " " << cols2 << endl;
 
-  // Declaración
-  vector< vector<float>> matrix2 (rows2, vector<float>(cols2, 0));
-  // Lectura.
-  for (int i = 0; i < rows2; ++i)
+  for (int i = 0; i < rows1; ++i){
     for (int j = 0; j < cols2; ++j)
-      ifs >> matrix2.at(i).at(j);
+      ofs << matrixf.at(i).at(j) << " ";
+    ofs << endl;
+  }
 
-  fs.close();
 
-  // Matriz producto.
-  vector< vector<float>> matrixf (rows1, vector<float>(cols2, 0));
-
+/*
   // Iniciamos entorno MPI
   MPI_Init(&argc, &argv);
 
@@ -79,6 +102,8 @@ int main(int argc, char *argv[]){
 
 
   MPI_Finalize();
+
+*/
   return(0);
 
 }
